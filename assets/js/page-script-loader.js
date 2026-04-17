fa61f19b18b3f901b64fb79d76d7f0d741d0129a
김예솔 <76675770+losey-kim@users.noreply.github.com>
2026-04-17 11:17:50 +0900

    단어 회상형 퀴즈 스크립트 로드 추가

--- assets/js/page-script-loader.js
+++ assets/js/page-script-loader.js
@@ -119,6 +119,7 @@
       "assets/js/app.js?v=33a15021",
       "assets/js/app-events.js",
       "assets/js/app-challenge-links.js",
+      "assets/js/app-vocab-recall.js",
       "assets/js/match-game.js?v=3f2e2e22",
       "assets/js/share-result.js?v=441dcdce",
       "assets/js/study-tools.js?v=9b778c4f"
